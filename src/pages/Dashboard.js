9126a1aad7fc412cf86403da613bd420ddc5e1ec
Jon Bennett <jonbennett@Jons-MacBook-Pro.local>
2023-11-27 19:21:11 -0500

    aider: Added sorting functionality to the trade table in the Dashboard component.
    # Aider chat conversation:
    
    USER: continue
    
    ASSISTANT: Certainly! Please let me know what specific changes or updates you would like to make to the *read-write* files you've listed. If you have any new features, bug fixes, or code refactoring in mind, I'm here to assist with the necessary code modifications.
    
    USER: continue the plan
    
    ASSISTANT: To continue with the development plan outlined in the `readme.md` file, the next steps would be to implement sorting and filtering functionality for the trade table in the `Dashboard.js` component. Let's start by implementing the sorting functionality.
    
    Here's how we can add sorting to the table:
    
    1. Add state to `Dashboard.js` to keep track of the current sort column and direction.
    2. Add a function to handle sorting when a column header is clicked.
    3. Update the rendering of the table to sort the trades based on the selected column and direction.
    
    Let's implement these changes:
    
    src/pages/Dashboard.js
    ```javascript
    <<<<<<< SEARCH
    import React, { useState, useEffect } from 'react';
    import { fetchOpenTrades } from '../services/metaApiService';
    =======
    import React, { useState, useEffect, useMemo } from 'react';
    import { fetchOpenTrades } from '../services/metaApiService';
    >>>>>>> REPLACE
    ```
    
    src/pages/Dashboard.js
    ```javascript
    <<<<<<< SEARCH
      const [openTrades, setOpenTrades] = useState([]);
    =======
      const [openTrades, setOpenTrades] = useState([]);
      const [sortConfig, setSortConfig] = useState({ key: null, direction: 'ascending' });
    
      const sortedTrades = useMemo(() => {
        let sortableTrades = [...openTrades];
        if (sortConfig.key !== null) {
          sortableTrades.sort((a, b) => {
            if (a[sortConfig.key] < b[sortConfig.key]) {
              return sortConfig.direction === 'ascending' ? -1 : 1;
            }
            if (a[sortConfig.key] > b[sortConfig.key]) {
              return sortConfig.direction === 'ascending' ? 1 : -1;
            }
            return 0;
          });
        }
        return sortableTrades;
      }, [openTrades, sortConfig]);
    
      const requestSort = (key) => {
        let direction = 'ascending';
        if (sortConfig.key === key && sortConfig.direction === 'ascending') {
          direction = 'descending';
        }
        setSortConfig({ key, direction });
      };
    >>>>>>> REPLACE
    ```
    
    src/pages/Dashboard.js
    ```javascript
    <<<<<<< SEARCH
                <th>Symbol</th>
                <th>Type</th>
                <th>Volume</th>
                <th>Profit</th>
                <th>Swap</th>
    =======
                <th onClick={() => requestSort('symbol')}>Symbol</th>
                <th onClick={() => requestSort('type')}>Type</th>
                <th onClick={() => requestSort('volume')}>Volume</th>
                <th onClick={() => requestSort('profit')}>Profit</th>
                <th onClick={() => requestSort('swap')}>Swap</th>
    >>>>>>> REPLACE
    ```
    
    src/pages/Dashboard.js
    ```javascript
    <<<<<<< SEARCH
              {openTrades.map((trade, index) => (
    =======
              {sortedTrades.map((trade, index) => (
    >>>>>>> REPLACE
    ```
    
    These changes will add sorting functionality to the table. When a user clicks on a column header, the table will sort by that column. Clicking the same column header again will toggle the sorting direction.
    
    Next, we would need to implement filtering functionality, which would involve adding input fields for the user to specify filter criteria and updating the table rendering to only show trades that match the filter. If you would like to proceed with that or any other feature, please let me know, and I can provide the necessary code changes.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { fetchOpenTrades } from '../services/metaApiService';
 
 function Dashboard() {
   const [openTrades, setOpenTrades] = useState([]);
+  const [sortConfig, setSortConfig] = useState({ key: null, direction: 'ascending' });
+
+  const sortedTrades = useMemo(() => {
+    let sortableTrades = [...openTrades];
+    if (sortConfig.key !== null) {
+      sortableTrades.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return sortableTrades;
+  }, [openTrades, sortConfig]);
+
+  const requestSort = (key) => {
+    let direction = 'ascending';
+    if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+      direction = 'descending';
+    }
+    setSortConfig({ key, direction });
+  };
 
   useEffect(() => {
     const loadOpenTrades = async () => {
@@ -19,15 +44,15 @@ function Dashboard() {
       <table>
         <thead>
           <tr>
-            <th>Symbol</th>
-            <th>Type</th>
-            <th>Volume</th>
-            <th>Profit</th>
-            <th>Swap</th>
+            <th onClick={() => requestSort('symbol')}>Symbol</th>
+            <th onClick={() => requestSort('type')}>Type</th>
+            <th onClick={() => requestSort('volume')}>Volume</th>
+            <th onClick={() => requestSort('profit')}>Profit</th>
+            <th onClick={() => requestSort('swap')}>Swap</th>
           </tr>
         </thead>
         <tbody>
-          {openTrades.map((trade, index) => (
+          {sortedTrades.map((trade, index) => (
             <tr key={index}>
               <td>{trade.symbol}</td>
               <td>{trade.type}</td>
